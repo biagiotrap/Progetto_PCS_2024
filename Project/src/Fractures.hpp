cfb99911d1902a8b6f9fca17766629fee6891ffb
Ayoube <s299921@studenti.polito.it>
2024-05-15 11:18:53 +0200

    aggiunti file header

---
+++ Project/src/Fractures.hpp
@@ -0,0 +1,29 @@
+#ifndef __FRACTURES_H__
+#define __FRACTURES_H__
+
+#include "Eigen/Eigen"
+#include <map>;
+
+using namespace std;
+using namespace Eigen;
+
+// Definiamo una struttura "fractures" dove importiamo le informazioni delle fratture all'interno dei file
+
+namespace fractureLibrary{
+
+struct Fractures
+{
+    unsigned int FractureNumber ;                     // intero positivo contiene il numero totale di fratture nel file
+    unsigned int VerticeNumber ;                    // intero positivo contiene per ogni frattura il numero di vertici
+    vector<unsigned int> Id ;                       // Vettore di interi positivi contiene l'identificativo di ogni frattura
+    vector<Vector3d> Coordinates ;                  // vettore 1xFractureNumber di vettori 3x1 di double. Contiene le coordinate dei vertici
+
+    unsigned int TracesNumber ;                     // Numero di traccia trovate
+    map<unsigned int, list<bool>> coupleIdTips ;    // mappa che contiene la coppia Id traccia e esito verifica della traccia(TIPS);
+
+    vector<double> LenghtsVector;                    // Vettore che contiene le lunghezze da Ordinare
+};
+
+}
+
+#endif
---
+++ Project/src/Utils.hpp
@@ -0,0 +1,39 @@
+#ifndef __UTILS__H_
+#define __UTILS_H_
+
+#include "Fractures.hpp"
+
+using namespace std;
+
+namespace fractureLibrary{
+
+// restituisce esito lettura dati, Vero se con successo, Falso altrimenti
+bool ImportData(const string &filePath, Fractures& fracture);
+
+
+// restituisce esito calcolo Traccia, e scrive risultato in fileOutput
+bool DefineTraces(const string &fileOutput, Fractures& fracture);
+
+
+// restituisce esito verifica (passante/non passante) mediante Tips e scrive risultato in file fileOutput
+bool CheckTrace(const string &fileOutput, Fractures& fracture) ;
+
+
+// restituisce un vettore che contiene le lughezze delle tracce
+bool ComputeLenghts(Fractures& fracture);
+
+// Ordinare usando l'algoritmo MergeSort--> da vedere quali parametri metterci dentro
+bool MergeSort();
+
+
+
+
+}
+
+
+
+
+
+
+
+#endif
