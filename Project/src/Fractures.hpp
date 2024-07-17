2039f585d08b15b9de2c7247e6785e8d66d78a9b
biagiotrap <s295894@studenti.polito.it>
2024-07-17 16:27:48 +0200

    salvataggio

--- Project/src/Fractures.hpp
+++ Project/src/Fractures.hpp
@@ -16,14 +16,16 @@ struct Fractures
     unsigned int FractureNumber ;                     // intero positivo contiene il numero totale di fratture nel file
     vector<unsigned int> VerticeNumber ;             //vettore di interi positivi contiene per ogni frattura il numero di vertici
     vector<unsigned int> Id ;                       // Vettore di interi positivi contiene l'identificativo di ogni frattura
-    vector<Vector3d> Coordinates ;                  // vettore 1xFractureNumber di vettori 3x1 di double. Contiene le coordinate dei vertici
-
-    unsigned int TracesNumber ;                     // Numero di traccia trovate
-    map<unsigned int, list<bool>> coupleIdTips ;    // mappa che contiene la coppia Id traccia e esito verifica della traccia(TIPS);
-
-    vector<double> LenghtsVector;                    // Vettore che contiene le lunghezze da Ordinare
+    vector<Vector3d> Coordinates ;                   // vettore 1xFractureNumber di vettori 3x1 di double. Contiene le coordinate dei vertici
+    vector<vector<Vector3d>> Segments;
 };
-
+struct Traces{
+    unsigned int TracesNumber=0 ;                     // Numero di traccia trovate
+    map<unsigned int, bool> CoupleIdTips ;    // mappa che contiene la coppia Id traccia e esito verifica della traccia(TIPS);
+    vector<Vector3d> TracesPoints;                  //vettore di vettori che contengono le coordinate degli estremi delle tracce
+    vector<double> LenghtsTrace;        // Vettore che contiene le lunghezze da Ordinare
+    vector<Vector2d> IdFractures;
+};
 }
 
 #endif
